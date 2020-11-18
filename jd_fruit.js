45b73bbdeeb39808235c99e0c0a8b320b0799b6f
songjiankme <73437812+songjiankme@users.noreply.github.com>
2020-11-18 11:18:15 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -104,12 +104,12 @@ async function jdFruit() {
       return
     }
     await doDailyTask();
-    await doTenWater();//浇水十次
+    //await doTenWater();//浇水十次
     await getFirstWaterAward();//领取首次浇水奖励
-    await getTenWaterAward();//领取10浇水奖励
+    //await getTenWaterAward();//领取10浇水奖励
     await getWaterFriendGotAward();//领取为2好友浇水奖励
     await duck();
-    await doTenWaterAgain();//再次浇水
+    //await doTenWaterAgain();//再次浇水
     await predictionFruit();//预测水果成熟时间
   } else {
     console.log(`初始化农场数据异常, 请登录京东 app查看农场0元水果功能是否正常,农场初始化数据: ${JSON.stringify($.farmInfo)}`);
